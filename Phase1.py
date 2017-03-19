92432b36b7a925732a9bff634a2ff3fcb33bb59b
David Monk <d.g.monk0@gmail.com>
2017-03-19 19:05:26 +0000

    Updated git repository.

--- Phase1.py
+++ Phase1.py
@@ -9,10 +9,19 @@ class Phase1:
         mlim = 6
         colors = iter(cm.rainbow(np.linspace(0, 1, mlim)))
         for i in range(1,mlim):
-            print i
-            g = BAGraph(N,pow(3,i))
-            g.generateGraph()
-            bins, dat = g.logBinDegrees()
+            data = []
+            B = np.zeros(0)
+            print "Testing m = ", pow(3,i)
+            for j in range(repeats):
+                g = BAGraph(N,pow(3,i))
+                g.generateGraph()
+                bins, dat = g.logBinDegrees()
+                if len(bins) > len(B):
+                    B = bins
+                d = np.zeros(100)
+                d[:len(dat)] = dat
+                data.append(d)
+            dat = np.mean(data, axis=0)[:len(bins)]
             plt.scatter(bins, dat, color=next(colors), label="m = %d"%(pow(3,i)))
         plt.xscale('log')
         plt.yscale('log')
@@ -22,11 +31,12 @@ class Phase1:
         plt.ylabel(r"$p\left( k \right)$", fontsize=18)
         plt.legend(loc=0)
         plt.savefig('./images/plot_m_1e4_%d'%repeats)
+        print "m test complete."
     def plotDifferentN(self, m, repeats=10):
-        Nlim = 6
+        Nlim = 7
         colors = iter(cm.rainbow(np.linspace(0, 1, Nlim)))
         for i in range(1,Nlim):
-            print i
+            print "Testing N = ", pow(10,i)
             data = []
             B = np.zeros(0)
             for j in range(repeats):
@@ -48,13 +58,14 @@ class Phase1:
         plt.ylabel(r"$p\left( k \right)$", fontsize=18)
         plt.legend(loc=0)
         plt.savefig('./images/plot_N_3_%d'%repeats)
+        print "N test complete."
     def plotGreatestDegree(self, m, repeats=10):
         Nlim = 17
         kn = []
         ksd = []
         for i in range(Nlim):
             k = []
-            print i
+            print "Testing greatest degree for N = ", pow(2,i)
             for j in range(repeats):
                 N = pow(2,i+1)
                 g = BAGraph(N,m)
@@ -69,16 +80,48 @@ class Phase1:
         plt.xlabel(r"$N$", fontsize=18)
         plt.ylabel(r"$k_1$", fontsize=18)
         plt.savefig('./images/plot_k_3_%d'%repeats)
-
+        print "k test complete."
+    def collapseData(self, m, repeats=10):
+        Nlim = 7
+        gamma = 3
+        colors = iter(cm.rainbow(np.linspace(0, 1, Nlim)))
+        for i in range(1,Nlim):
+            print "Collapsing N = ", pow(10,i)
+            data = []
+            B = np.zeros(0)
+            for j in range(repeats):
+                g = BAGraph(pow(10,i),m)
+                g.generateGraph()
+                bins, dat = g.logBinDegrees()
+                if len(bins) > len(B):
+                    B = bins
+                d = np.zeros(100)
+                d[:len(dat)] = dat
+                data.append(d)
+            dat = np.mean(data, axis=0)[:len(bins)]
+            for j in range(len(dat)):
+                dat[j] = pow(bins[j],gamma)*dat[j]
+            plt.scatter(np.array(bins)/np.sqrt(pow(10,i)), dat, color=next(colors), label="N = %d"%(pow(10,i)))
+        plt.xscale('log')
+        plt.yscale('log')
+        plt.ylim(1e-5,1e4)
+        #plt.xlim(1,1e4)
+        plt.xlabel(r"$k$", fontsize=18)
+        plt.ylabel(r"$p\left( k \right)$", fontsize=18)
+        plt.legend(loc=3)
+        plt.savefig('./images/collapse_3_%d'%repeats)
+        print "Data collapse complete."
 
 def main():
     p = Phase1()
     if (sys.argv[1] == 'm'):
-        p.plotDifferentm(pow(10,4), repeats = 10)
+        p.plotDifferentm(pow(10,4), repeats = int(sys.argv[2]))
     elif (sys.argv[1] == 'N'):
-        p.plotDifferentN(3, repeats = 10)
+        p.plotDifferentN(3, repeats = int(sys.argv[2]))
     elif (sys.argv[1] == 'k'):
-        p.plotGreatestDegree(3, repeats = 10)
+        p.plotGreatestDegree(3, repeats = int(sys.argv[2]))
+    elif (sys.argv[1] == 'c'):
+        p.collapseData(3, repeats = int(sys.argv[2]))
 
 if (__name__ == '__main__'):
     main()
--- gen_results.sh
+++ gen_results.sh
@@ -1,3 +1,6 @@
-python Phase1.py m &
-python Phase1.py N &
-python Phase1.py k
+python Phase1.py m $1 &
+python Phase1.py N $1 &
+python Phase1.py k $1 &
+python Phase1.py c $1 &
+
+wait
